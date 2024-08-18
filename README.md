37b5e0b98990fa85b3fb1137b0dbc7a51685d873
arnoldindia <igrowsolutionstn@gmail.com>
2024-08-18 19:53:16 +0530

    Update on 18th Aug 2024

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PyL1Assignment02
 2nd Pyhton assignment as part of Level 1 Python training
+
+
+Given a list which may contain values with duplicates, print all elements and their frequencies.
+
---
+++ frequencyCount.py
@@ -0,0 +1,10 @@
+def countFrequency(values, n):
+    
+    
+    print()
+
+
+if __name__ == '__main__':
+    values = [10, 20, 20, 10, 10, 20, 5, 20]
+    size = len(values)
+    countFrequency(values, size)
